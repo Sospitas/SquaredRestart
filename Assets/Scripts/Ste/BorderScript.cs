dd3253a565147a2886901051e4484085fed626ea
Steven Brown <stevenbrown_@hotmail.co.uk>
2014-07-18 16:38:27 +0100

    This build doesn't work.
    I have added the first level in with velocity movement. The values will
    need to be changed for this. Also I have added the borders and am in the
    middle of getting everything to work with 2D physics.

---
+++ Assets/Scripts/Ste/BorderScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngineInternal;
+
+public class BorderScript : MonoBehaviour 
+{
+    public GameObject LeftBorder, RightBorder, TopBorder, BottomBorder;
+    private float offsetValue = 2.0f;  
+    //When an object enters the border it will be moved to the other side of the screen
+    //respectively.
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            if(this.tag == "LeftBorder")
+            {
+                float x = RightBorder.transform.position.x - RightBorder.collider2D.bounds.extents.x/2 - offsetValue;
+                col.transform.position = new Vector2(x, col.transform.position.y);
+            }
+            else if(this.tag == "RightBorder")
+            {
+                float x = LeftBorder.transform.position.x + LeftBorder.collider2D.bounds.extents.x/2 + offsetValue;
+                col.transform.position = new Vector2(x, col.transform.position.y);
+            }
+            else if(this.tag == "TopBorder")
+            {
+                float y = BottomBorder.transform.position.y + BottomBorder.collider2D.bounds.extents.y/2 + offsetValue;
+                col.transform.position = new Vector2(col.transform.position.x, y);
+            }
+            else if(this.tag == "BottomBorder")
+            {
+                float y = TopBorder.transform.position.y - TopBorder.collider2D.bounds.extents.y/2 - offsetValue;
+                col.transform.position = new Vector2(col.transform.position.x, y);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Ste/MovementScript.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	public Vector2 topVel;
+	private Vector2 velocity;
+    private Vector2 targetVel;
+    private bool speedUp;
+	// Use this for initialization
+	void Start () 
+	{
+		velocity = new Vector2(0.0f, 0.0f);
+        targetVel = new Vector2(0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		UpdateInputs ();
+	}
+
+    void LateUpdate()
+    {
+        UpdateMovement();
+    }
+
+    void UpdateMovement()
+    {
+        if (speedUp)
+        {
+            targetVel = new Vector2(velocity.x, this.rigidbody2D.velocity.y);
+            this.rigidbody2D.velocity = Vector2.Lerp(this.rigidbody2D.velocity, targetVel, 120 * Time.deltaTime);
+        }
+        else
+        {
+            targetVel = new Vector2(0.0f, this.rigidbody2D.velocity.y);
+            this.rigidbody2D.velocity = Vector2.Lerp(this.rigidbody2D.velocity, targetVel, 10 * Time.deltaTime);
+        }
+
+
+    }
+
+	void UpdateInputs()
+	{
+        //These are the controls for PC
+        //MoveRight
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (velocity.x < topVel.x)
+            {
+                velocity.x += 2 * Time.deltaTime;
+                speedUp = true;
+                if (velocity.x > topVel.x)
+                {
+                    velocity.x = topVel.x;
+                }
+            }
+        }
+        //Move Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (velocity.x > -topVel.x)
+            {
+                velocity.x -= 2 * Time.deltaTime;
+                speedUp = true;
+                if (velocity.x < -topVel.x)
+                {
+                    velocity.x = -topVel.x; 
+                }
+            }
+        }
+        //Move the velocity back to 0.
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            speedUp = false; 
+            velocity.x = 0;
+        }
+    }
+}
